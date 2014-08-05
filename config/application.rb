5b5241256b69cb2c994cc80b57d3d572dde0cee8
Sean McCleary <seanmcc@gmail.com>
2014-08-05 12:48:09 -0700

    renaming application namespace and adding rspec config options

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,17 @@ require "action_mailer/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Vcr
+module VcrRails
   class Application < Rails::Application
+
+    config.generators do |gen|
+      gen.test_framework :rspec,
+                         fixtures: true,
+                         view_specs: false,
+                         helper_specs: false,
+                         routing_specs: false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
